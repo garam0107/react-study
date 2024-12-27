d155de575b7aa6806eee7f4a4faadb40c9e79a2e
garam0107 <name3937@gmail.com>
2024-12-27 23:22:57 +0900

    1227 react study

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function Header() {
+  return(
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <div>안녕하세요</div>
+    </div>
+  );
+}
+
+export default App;
